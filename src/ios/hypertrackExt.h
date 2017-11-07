c2236e3d4e0b53c5305c3ef3e3d2fa6ec85502ce
cnsimbe <cnsimbe@sfu.ca>
2017-11-07 14:23:45 -0800

    removed ios support

--- src/ios/hypertrackExt.h
+++
@@ -1,8 +0,0 @@
-#import "AppDelegate.h"
-
-@interface AppDelegate (notification)
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions;
-- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(nonnull NSData *)deviceToken;
-- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(nonnull NSError *)error;
-- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo;
-@end
