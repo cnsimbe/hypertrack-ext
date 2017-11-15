11f005e419f6e311c16777b4136833d80a2d311d
cnsimbe <cnsimbe@sfu.ca>
2017-11-15 09:42:51 -0800

    success callback

--- src/android/hyperTrack.java
+++ src/android/hyperTrack.java
@@ -16,6 +16,7 @@ public class hyperTrack extends CordovaPlugin {
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("disablePushNotification")) {
             HyperTrack.disablePersistentNotification(true);
+            callbackContext.success("success");
             return true;
         }
         return false;
